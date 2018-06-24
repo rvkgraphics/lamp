11a081a71b8e0007b00a30e4455776d024ec9f79
Avdhut Pandit <apandit@tavisca.com>
2018-06-24 13:15:11 +0530

    deleted the code

--- index.php
+++ index.php
@@ -6,26 +6,6 @@
                                     <body>
                                       <h1>Welcome to PHP Deployment 1</h1>
                                      <?php
-// Setup a handle for CURL
-$curl_handle = curl_init();
-curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
-curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-// Get the hostname of the intance from the instance metadata
-curl_setopt($curl_handle, CURLOPT_URL, 'http://169.254.169.254/latest/meta-data/public-hostname');
-$hostname = curl_exec($curl_handle);
-if (empty($hostname)) {
-    print "Sorry for some reason we got no hostname back <br />";
-} else {
-    print "Server =  . $hostname . <br />";
-}
-// Get the instance-id of the intance from the instance metadata
-curl_setopt($curl_handleCURLOPT_URL, 'http://169.254.169.254/latest/meta-data/instance-id');
-$instanceid = curl_exec($curl_handle);
-if (empty($instanceid)) {
-    print "Sorry for some reason we got no instance id back <br />";
-} else {
-    print "EC2 instance-id =  . $instanceid . <br />";
-}
 $Database   = "mysql.lampstack.com";
 $DBUser     = "lamp";
 $DBPassword = "lamp1234";
